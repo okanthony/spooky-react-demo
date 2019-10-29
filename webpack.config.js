873627908c9049ece7769e4cb9cbf92ffabe193f
Anthony DeGennaro <degennaro.anthony@gmail.com>
2019-10-29 12:56:49 -0400

    add: webpack config

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    stats: 'errors-only',
+    overlay: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [['@babel/plugin-proposal-class-properties', { loose: true }]],
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            cacheDirectory: true
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+};
